4b0d99e7cc8b461e23341dfe5ea1eddbde4a2ec6
unknown <sooryamsns@gmail.com>
2026-02-05 22:59:15 +0530

    day 5 done

---
+++ JS/day5.js
@@ -0,0 +1,27 @@
+// let a = 10;
+// let b = 10;
+// let c = 7;
+
+// if(a >= b && a>= c){
+//     console.log("a is greatest")
+// }else if(b >= a && b >= c){
+//     console.log("b is greatest")
+// } else{
+//     console.log("c is greatest")
+// }
+
+// let str = "sooryams";
+// let count = 0;
+// let vowels = "aeiou";
+
+// for (let i = 0; i < str.length; i++) {
+//   if (vowels.includes(str[i])) {
+//   count ++}
+// }
+
+// console.log(count);
+
+
+for(i=1; i<11; i++){
+    console.log(i)
+}
--- react/counter/src/Input.jsx
+++ react/counter/src/Input.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const Input = () => {
   const [text, setText] = useState("");
-  const [showText,setShowText] = useState("")
+  const [showText, setShowText] = useState("");
   return (
     <div>
       <input
@@ -11,8 +11,21 @@ const Input = () => {
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
-      <button className="border ml-3" onClick={()=> setShowText(text)}>Click me</button>
+      <button className="border ml-3" onClick={() => setShowText(text)}>
+        Click me
+      </button>
+      <p>{text} </p>
       <p>{showText} </p>
+      <button
+        className="border ml-3"
+        onClick={() => {
+          setText("");
+          setShowText("");
+        }}
+      >
+        {" "}
+        clear{" "}
+      </button>
     </div>
   );
 };
